ea6d1ca602b3d1975164fdeb22f73c4fd8e2de2f
white9300 <rain930624@gmail.com>
2021-03-22 13:15:33 +0100

    autok objektum

--- fogyasztas/fogyasztas.js
+++ fogyasztas/fogyasztas.js
@@ -1,3 +1,4 @@
+
 let km = 0
 
 do {
@@ -10,4 +11,15 @@ do {
   tankolas = prompt("Hány litert tankoltál?", 0);
 } while (tankolas >= 50 || tankolas <= 1);
 
-let autok = {evjarat:"2007", marka:"Suzuki"};
+
+let autok = {
+    evjarat:"2007",
+    marka:"Suzuki",
+    utca:"Szabadság",
+    fogyaszt:   function(tan,km){
+        let fogy = (tan/km)*100
+        return this.evjarat + " évjáratú " + this.marka + " fogyasztása " + fogy
+    }
+   
+}
+console.log(autok.fogyaszt(tankolas,km))
