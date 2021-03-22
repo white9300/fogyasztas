1789f8d0ada2509f53f6f6a5e85ae5969b5ab2d6
white9300 <rain930624@gmail.com>
2021-03-22 12:06:11 +0100

    7. feladat

--- fogyasztas/fogyasztas.js
+++ fogyasztas/fogyasztas.js
@@ -1,5 +1,11 @@
-let km
+let km = 0
 
 do {
   km = prompt("Hány kilómétert mentél?", 0);
-} while (km >= 1000 && km);
+} while (km >= 1000 || km <= 1 );
+
+let tankolas   
+
+do {
+  tankolas = prompt("Hány litert tankoltál?", 0);
+} while (tankolas >= 50 || tankolas <= 1);
